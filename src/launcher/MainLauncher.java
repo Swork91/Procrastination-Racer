bfa54997566885ea45021d8b24875e35c8754b65
Swork91 <chicagomike999@gmail.com>
2018-04-11 16:55:06 -0400

    THE CAR GOES AND ITS BEAUTIFUL

--- src/launcher/MainLauncher.java
+++ src/launcher/MainLauncher.java
@@ -1,21 +1,22 @@
 package launcher;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import javax.swing.*;
-//My Stuff
-import cars.Vehicle;
-import displayFrames.*;
-
 
 public class MainLauncher extends JFrame{
 static final private int version = 0; //0 for beta I guess
-static final private int patch = 3;
+static final private int patch = 4;
+private RaceTrack raceTrack = new RaceTrack();
 
 public MainLauncher() {
-	add(new RaceTrack(), BorderLayout.CENTER);
+	add(raceTrack, BorderLayout.CENTER);
 	
 	JPanel hud = new RaceInfoHUD();
 	hud.setLayout(new GridLayout(2,0));
 	add(hud, BorderLayout.EAST);
+	
+	raceTrack.setFocusable(true);
 }
 
 	public static void main(String[] args) {
@@ -26,6 +27,48 @@ public MainLauncher() {
 		//mainWindow.pack();
 		mainWindow.setLocationRelativeTo(null);
 		mainWindow.setVisible(true);
+		
 	}
+	
+	static class RaceTrack extends JPanel {
+		private Image raceTrackImage = new ImageIcon("images\\raceTrackBG1.png").getImage();
+	    private Image carImage = new ImageIcon("images\\car1.png").getImage();
 
+		private int carXPos = 0;
+		private int carYPos = 0;
+		private int speed = 50;
+		
+		public RaceTrack() {
+			addKeyListener(new KeyAdapter() {
+				public void keyPressed(KeyEvent e) {
+					switch (e.getKeyCode()) {
+					case KeyEvent.VK_DOWN: carYPos+=speed; break;
+					case KeyEvent.VK_UP: carYPos-=speed; break;
+					case KeyEvent.VK_LEFT: carXPos-=speed; break;
+					case KeyEvent.VK_RIGHT: carXPos+=speed; break;
+					}
+					repaint();
+					System.out.println("xpos: "+carXPos+"\nypos: "+carYPos+"\n");
+				}
+			});
+		}
+		
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			
+			g.setColor(Color.BLACK);
+			g.fillRect(0, 0, getWidth(), getHeight());
+	        g.drawImage(raceTrackImage, 0, 0, getWidth(), getHeight(), this);
+			g.drawImage(carImage, carXPos, carYPos, carImage.getWidth(null), carImage.getHeight(null), this);
+	        //g.drawImage(carImage, carXPos, carYPos, carXPos+100, carYPos+200, this);
+		}
+	}
+	
+	public class RaceInfoHUD extends JPanel {
+		
+		public RaceInfoHUD() {
+			add(new JLabel("Time: 00:00.000"));
+			add(new JLabel("Lap: 0"));
+		}
+	}
 }
