dc0d0fbf5e2b9c492427e08f013d07bba871f6d0
Swork91 <chicagomike999@gmail.com>
2018-04-12 15:31:24 -0400

    0.6 car restricted to track now
    /*************************** Patch Notes
    *********************************
    * Car stays on the track now baby. AND I didn't scrap the layout idea.
    * We're still using layouts! Platform independence for life!
    * Also I created a puddle that I thought would make a fun obstacle since
    there is no other players.
    
    ************************************************************************/

--- src/launcher/MainLauncher.java
+++ src/launcher/MainLauncher.java
@@ -15,7 +15,7 @@ private RaceInfoHUD hud = new RaceInfoHUD();
 public MainLauncher() {
 	add(raceTrack, BorderLayout.CENTER);
 	add(hud, BorderLayout.EAST);
-	hud.setLayout(new GridLayout(2,0));	
+	hud.setLayout(new GridLayout(3,0));	
 	raceTrack.setFocusable(true);
 }
 
@@ -25,6 +25,7 @@ public MainLauncher() {
 		mainWindow.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		mainWindow.setSize(1920, 1080);
 		//mainWindow.pack();
+		mainWindow.setResizable(false);
 		mainWindow.setLocationRelativeTo(null);
 		mainWindow.setVisible(true);
 		
@@ -37,18 +38,42 @@ public MainLauncher() {
 		private Image raceTrackImage = new ImageIcon("images\\raceTrackBG1.png").getImage();
 	    private Image carImage = new ImageIcon("images\\car1.png").getImage();
 
-		private int carXPos = 0;
-		private int carYPos = 0;
-		private int speed = 50;
+		private int carXPos = 100;
+		private int carYPos = 100;
+		private int speed = 50;//don't go too fast or you'll clip thorugh stuff
 		
 		public RaceTrack() {
 			addKeyListener(new KeyAdapter() {
 				public void keyPressed(KeyEvent e) {
 					switch (e.getKeyCode()) {
-					case KeyEvent.VK_DOWN: carYPos+=speed; break;
-					case KeyEvent.VK_UP: carYPos-=speed; break;
-					case KeyEvent.VK_LEFT: carXPos-=speed; break;
-					case KeyEvent.VK_RIGHT: carXPos+=speed; break;
+					case KeyEvent.VK_DOWN: 
+						if (carYPos>=900) {
+							break;
+						}
+						else {
+							carYPos+=speed; break;
+						}
+					case KeyEvent.VK_UP: 
+						if (carYPos<=50) {
+							break;
+						}
+						else {
+							carYPos-=speed; break;
+						}
+					case KeyEvent.VK_LEFT: 
+						if (carXPos<=50) {
+							break;
+						}
+						else {
+							carXPos-=speed; break;
+						}
+					case KeyEvent.VK_RIGHT: 
+						if (carXPos>=1650) {
+							break;
+						}
+						else {
+							carXPos+=speed; break;
+						}
 					}
 					repaint();
 					System.out.println("xpos: "+carXPos+"\nypos: "+carYPos+"\n");
@@ -59,10 +84,16 @@ public MainLauncher() {
 		protected void paintComponent(Graphics g) {
 			super.paintComponent(g);
 			
-			g.setColor(Color.BLACK);
+			g.setColor(Color.GREEN);
 			g.fillRect(0, 0, getWidth(), getHeight());
+			
 	        g.drawImage(raceTrackImage, 0, 0, getWidth(), getHeight(), this);
-			g.drawImage(carImage, carXPos, carYPos, carImage.getWidth(null), carImage.getHeight(null), this);
+	        //g.drawImage(carImage, carXPos, carYPos, carImage.getWidth(null), carImage.getHeight(null), this);
+	        g.drawImage(carImage, carXPos, carYPos, 100, 100, this);
+	        g.setColor(Color.GREEN);
+	        g.fillOval(getWidth()*3/8, getHeight()*3/8, getWidth()/4, getHeight()/4);
+	        g.setColor(Color.BLUE);
+	        g.fillOval(getWidth()/4, getHeight()/4, getWidth()/8, getHeight()/8);
 		}
 	}
 	/**********
@@ -74,6 +105,7 @@ public MainLauncher() {
 			Timer timer = new Timer(1, new TimerListener());
 			timer.start();
 			
+			add(new JLabel("Procrastination RACER"));
 			add(seconds);
 			add(new JLabel("Lap: 0"));
 		}
@@ -81,13 +113,13 @@ public MainLauncher() {
 		class TimerListener implements ActionListener{
 			private int timeMiliSeconds = 0;
 			public void actionPerformed(ActionEvent e) {
-				seconds.setText("00:"+timeMiliSeconds/1000+"."+(timeMiliSeconds++%1000));			
+				seconds.setText((timeMiliSeconds/1000)/60+":"+(timeMiliSeconds/1000)%60+"."+(timeMiliSeconds++%1000));
 			}
 		}
 		
 		protected void paintComponent(Graphics g) {
 			super.paintComponent(g);
-			
+			g.setFont(new Font("TimesRoman", Font.PLAIN, 24));
 			g.setColor(Color.WHITE);
 			g.fillRect(0, 0, 200, 1);
 		}
